2d288fc279310f4769ae36fcda075171b18b2a1a
Petr Urban <urbanp@gapps.zcu.cz>
2023-03-09 18:09:23 +0100

    Initial commit

---
+++ src/main/java/cz/kiv/vi/datavisualization/DataVisualizationApplication.java
@@ -0,0 +1,13 @@
+package cz.kiv.vi.datavisualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataVisualizationApplication.class, args);
+	}
+
+}
